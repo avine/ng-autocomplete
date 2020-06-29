160be498886290875b98bb585ab106f91fb3536c
Stéphane Francel <contact@avine.io>
2020-06-30 00:19:38 +0200

    Add autocomplete lib and demo app

---
+++ projects/autocomplete/src/lib/autocomplete.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'avn-autocomplete',
+  template: `
+    <p>
+      autocomplete works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AutocompleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
